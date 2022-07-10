fb75f4a3a7adbb6e0e54a8c5a03f94845e2800d6
anushikasrivastava110 <79199195+Anushika-Srivastava@users.noreply.github.com>
2022-07-10 18:15:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let input = document.querySelector('button');
+
+input.addEventListener('click', ()=>{
+    window.open("http://meet.google.com/new", '_blank')
+    // window.open("http://meet.google.com/new", '_blank', 'height:800px, width:800px')  // to create a new window of size 800x800
+});
